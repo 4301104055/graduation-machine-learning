62f4b339f9cefa7da308e3094fcafd947f9eab39
NGUYEN THI DIEU HOAN <4301104055@student.hcmup.edu.vn>
2021-04-30 12:28:38 +0700

    updated

--- js/single-images-demo.js
+++ js/single-images-demo.js
@@ -403,3 +403,38 @@ var getimage = function(){
     image_prview_show.attr("src", rhinoStorage)
   }
 }
+$(function() {
+  $('.upload-video-file').on('change', function(){
+    if (isVideo($(this).val())){
+      $('.video-preview').attr('src', URL.createObjectURL(this.files[0]));
+      $('.video-prev').show();
+    }
+    else
+    {
+      $('.upload-video-file').val('');
+      $('.video-prev').hide();
+      alert("Only video files are allowed to upload.")
+    }
+  });
+});
+
+// If user tries to upload videos other than these extension , it will throw error.
+function isVideo(filename) {
+  var ext = getExtension(filename);
+  switch (ext.toLowerCase()) {
+  case 'm4v':
+  case 'avi':
+  case 'mp4':
+  case 'mov':
+  case 'mpg':
+  case 'mpeg':
+  case 'mkv':
+      return true;
+  }
+  return false;
+}
+
+function getExtension(filename) {
+  var parts = filename.split('.');
+  return parts[parts.length - 1];
+}
